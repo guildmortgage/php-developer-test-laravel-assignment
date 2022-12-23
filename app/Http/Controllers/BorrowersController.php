b660ea403b0c45422150c96fae6ff9265dffeb57
alan_sheffield <a92rttt@hotmail.com>
2022-12-23 09:56:01 -0500

    create api controller and update route

---
+++ app/Http/Controllers/BorrowersController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Borrowers;
+use Illuminate\Http\Request;
+
+class BorrowersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $loans = Borrowers::leftjoin('employers','employers.borrower_id', '=', 'borrowers.id')
+                    ->leftjoin('bank_accounts','bank_accounts.borrower_id', '=', 'borrowers.id')
+                    ->selectRaw('borrowers.name, sum(IFNULL(employers.income,0) + IFNULL(bank_accounts.balance,0)) as totalIincome')
+                    ->groupBy('borrowers.name')
+                    ->orderBy('borrowers.name')
+                    ->get();
+       
+        return [
+            "status" => 1,
+            "data" => $loans,
+        ];
+    }
+}
