4908d58d755ba26555f4ff561671972fc924192d
pitt <pitt@jss.hu>
2023-08-03 05:14:34 -0700

    Create the basic Models, Seeders, Factories

---
+++ database/factories/BankAccountFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BankAccount>
+ */
+class BankAccountFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'account_number'    => fake()->numberBetween('1111111111','999999999999'),
+            'balance'    => fake()->randomFloat('2',55000,300000),
+        ];
+    }
+}
---
+++ database/factories/JobFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title'     => 'Job '.fake()->randomNumber('3'),
+            'annual_salary'     => fake()->randomFloat('2',55000,300000),
+        ];
+    }
+}
